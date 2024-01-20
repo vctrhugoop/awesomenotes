74028b8643c89152539d43fdc3bf068781f6159b
vctrhugoop <vhdestroy@hotmail.com>
2024-01-19 22:08:20 -0300

    ✨ feat: Add state management for links in component

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import { FiArrowLeft } from 'react-icons/fi';
 import { FaLink, FaTags } from 'react-icons/fa';
 
 import { Container, Form } from './styles';
-import { Link } from 'react-router-dom';
 
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
@@ -13,6 +15,18 @@ import { Section } from '../../components/Section';
 import { Button } from '../../components/Button';
 
 export function New() {
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState('');
+
+  function handleAddLink() {
+    setLinks(prevState => [...prevState, newLink]);
+    setNewLink('');
+  }
+
+  function handleDeleteLink(deleted) {
+    setLinks(prevState => prevState.filter(link => link !== deleted));
+  }
+
   return (
     <Container>
       <Header />
@@ -28,8 +42,21 @@ export function New() {
           <Textarea placeholder="Observações" />
 
           <Section icon={FaLink} title="Links úteis">
-            <NoteItem value="Exemplo 1" />
-            <NoteItem isNew placeholder="Novo Link" />
+            {links.map((link, index) => (
+              // eslint-disable-next-line react/jsx-key
+              <NoteItem
+                key={String(index)}
+                value={link}
+                onClick={() => handleDeleteLink(link)}
+              />
+            ))}
+            <NoteItem
+              isNew
+              placeholder="Novo Link"
+              value={newLink}
+              onChange={e => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
           <Section icon={FaTags} title="Marcadores">
