34c79dd8a85a17015d5cc0d1be5ac7d3b67779a5
vctrhugoop <vhdestroy@hotmail.com>
2024-01-20 10:44:47 -0300

    ✨ feat: loading notas data

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 import { PiTrashBold } from 'react-icons/pi';
 import { FaLink, FaTags } from 'react-icons/fa';
 
@@ -9,43 +12,58 @@ import { ButtonText } from '../../components/ButtonText';
 import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
 
+import api from '../../services/api';
+
 export function Details() {
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+
+  useEffect(() => {
+    async function fetchNotes() {
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+    fetchNotes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Container>
       <Header />
 
-      <main>
-        <Content>
-          <ButtonText title="Excluir nota" isActive icon={PiTrashBold} />
-          <h1>Método Pomodoro</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-            perspiciatis odio aliquam voluptatem voluptas dolores voluptate
-            incidunt officia quos, quis veritatis rerum ratione magni eum error
-            numquam sapiente beatae sunt!
-          </p>
-          <Section icon={FaLink} title="Links úteis">
-            <Links>
-              <li>
-                <a
-                  href="https://pomofocus.io/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  https://pomofocus.io/
-                </a>
-              </li>
-            </Links>
-          </Section>
-          <Section icon={FaTags} title="Marcadores">
-            <Tag title="Foco" />
-            <Tag title="Agil" />
-          </Section>
-          <a href="/">
-            <Button title="Voltar" />
-          </a>
-        </Content>
-      </main>
+      {data && (
+        <main>
+          <Content>
+            <ButtonText title="Excluir nota" isActive icon={PiTrashBold} />
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+            {data.links && (
+              <Section icon={FaLink} title="Links úteis">
+                <Links>
+                  {data.links.map(link => (
+                    <li key={String(link.id)}>
+                      <a href={link.url} target="_blank" rel="noreferrer">
+                        {link.url}
+                      </a>
+                    </li>
+                  ))}
+                </Links>
+              </Section>
+            )}
+            {data.tags && (
+              <Section icon={FaTags} title="Marcadores">
+                {data.tags.map(tag => (
+                  <Tag key={String(tag.id)} title={tag.name} />
+                ))}
+              </Section>
+            )}
+            <a href="/">
+              <Button title="Voltar" />
+            </a>
+          </Content>
+        </main>
+      )}
     </Container>
   );
 }
